32a9747b830cc5a0de44ae84f33b15262ef9af0c
TharwatJ6 <tharwatayman56@gmail.com>
2026-02-17 10:01:26 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(1, 101);
+            int guess;
+            Console.WriteLine("guess a number from 1-100");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if(guess>number)
+            {
+                Console.WriteLine("too high");
+            }
+            else if(guess<number)
+            {
+                Console.WriteLine("too low");
+            }
+            else
+            {
+                Console.WriteLine("you win");
+            }
+        }
+    }
+}
