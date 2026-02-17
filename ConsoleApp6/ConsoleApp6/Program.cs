52198eec92aa1044a995ec91a4406a4f4cac6997
TharwatJ6 <tharwatayman56@gmail.com>
2026-02-17 10:02:58 +0200

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, total = 1;
+            Console.WriteLine("write the number to calculate factorial");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                total = total * (i + 1);
+            }
+            Console.WriteLine("the factorial of " + number + " is " + total);
+        }
+    }
+}
